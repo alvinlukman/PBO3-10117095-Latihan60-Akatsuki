d518e341f52adba189568c733b571840232b3af2
alvinlukmanNH <galinx20gmail.com>
2018-10-30 00:25:41 +0700

    akatsuki

---
+++ src/Akatsuki.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * Nama        : Alvin Lukman Nulhakim
+ * Nim         : 10117095
+ * Kelas       : IF-3
+ */
+public class Akatsuki {
+    
+    protected String clothes;
+    protected String goals;
+
+    public Akatsuki(String clothes, String goals) {
+        this.clothes = clothes;
+        this.goals = goals;
+    }
+
+    public String getClothes() {
+        return clothes;
+    }
+
+    public String getGoals() {
+        return goals;
+    }
+}
